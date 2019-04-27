58f1a92350c5ded488298303fa00309a05eb3ff5
Sagar Kunwar Chhetri <36431491+skunwarc@users.noreply.github.com>
2019-04-27 16:06:26 -0400

    Update Case.sql

--- Case.sql
+++ Case.sql
@@ -5,11 +5,22 @@ SELECT player_name,
             ELSE 'no' END AS is_a_senior
   FROM benn.college_football_players
 
+
 //multiple condition
 SELECT player_name,
        weight,
        CASE WHEN weight > 250 THEN 'over 250'
             WHEN weight > 200 THEN '201-250'
             WHEN weight > 175 THEN '176-200'
+            ELSE '175 or under' END AS weight_group
+  FROM benn.college_football_players
+  
+  
+//better way for multiple condition, since above query is little confusing for when weight >250 and weight >200
+SELECT player_name,
+       weight,
+       CASE WHEN weight > 250 THEN 'over 250'
+            WHEN weight > 200 AND weight <= 250 THEN '201-250'
+            WHEN weight > 175 AND weight <= 200 THEN '176-200'
             ELSE '175 or under' END AS weight_group
   FROM benn.college_football_players
