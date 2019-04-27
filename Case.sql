c3c1e08d5dea58ef46c7ecb27787d877e5233183
Sagar Kunwar Chhetri <36431491+skunwarc@users.noreply.github.com>
2019-04-27 16:02:52 -0400

    Create Case.sql

---
+++ Case.sql
@@ -0,0 +1,15 @@
+//single condition
+SELECT player_name,
+       year,
+       CASE WHEN year = 'SR' THEN 'yes'
+            ELSE 'no' END AS is_a_senior
+  FROM benn.college_football_players
+
+//multiple condition
+SELECT player_name,
+       weight,
+       CASE WHEN weight > 250 THEN 'over 250'
+            WHEN weight > 200 THEN '201-250'
+            WHEN weight > 175 THEN '176-200'
+            ELSE '175 or under' END AS weight_group
+  FROM benn.college_football_players
