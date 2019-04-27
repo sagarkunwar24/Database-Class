91858c7f59d3e4048975c19a0279ed2236614f20
Sagar Kunwar Chhetri <36431491+skunwarc@users.noreply.github.com>
2019-04-27 16:32:55 -0400

    Create Join.sql

---
+++ Join.sql
@@ -0,0 +1,5 @@
+• LEFT JOIN returns only unmatched rows from the left table.
+• RIGHT JOIN returns only unmatched rows from the right table.
+• FULL OUTER JOIN returns unmatched rows from both tables, which is same as Union.
+
+//For more clear visualization: http://joins.spathon.com/
